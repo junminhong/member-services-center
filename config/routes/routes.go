7a9a28f350cb3ab87778ca8091781761b1bd2640
junminhong <junminhong1110@gmail.com>
2021-12-15 18:56:52 +0800

    在routes內新增cros相關設定

--- config/routes/routes.go
+++ config/routes/routes.go
@@ -6,9 +6,26 @@ import (
 	"sync"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func InitRoutes(apiVersion string, intiServerWg *sync.WaitGroup) {
 	defer intiServerWg.Done()
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 	var apiVersionTmp *gin.RouterGroup
 	switch apiVersion {
 	case "v1", "V1":
