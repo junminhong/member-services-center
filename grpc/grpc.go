8354758c08afa693a3f53b428f6b80ccd3bc3911
junminhong <junminhong1110@gmail.com>
2022-01-21 21:02:13 +0800

    update grpc func，add zap logger

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -2,34 +2,45 @@ package grpc
 
 import (
 	"context"
+	"github.com/junminhong/member-services-center/db/redis"
 	"github.com/junminhong/member-services-center/grpc/proto"
 	"github.com/junminhong/member-services-center/pkg/jwt"
+	"github.com/junminhong/member-services-center/pkg/logger"
 	"google.golang.org/grpc"
-	"log"
 	"net"
 	"sync"
 )
 
+var sugar = logger.Setup()
+var redisClient = redis.Setup()
+
 type Server struct{}
 
 func (s Server) VerifyAccessToken(ctx context.Context, request *proto.TokenAuthRequest) (*proto.TokenAuthResponse, error) {
-	response := &proto.TokenAuthResponse{Response: jwt.VerifyAccessToken(request.Token)}
+	if !jwt.VerifyAccessToken(request.Token) {
+		return nil, nil
+	}
+	memberID, err := redisClient.Get(context.Background(), request.Token).Result()
+	if err != nil {
+		sugar.Info(err.Error())
+	}
+	response := &proto.TokenAuthResponse{MemberID: memberID}
 	return response, nil
 }
 
 func SetupServer(intiServerWg *sync.WaitGroup) {
 	defer intiServerWg.Done()
-	log.Println("starting gRPC server...")
-	log.Println("Listening and serving HTTP on :127.0.0.1:2021")
+	sugar.Info("starting gRPC server...")
+	sugar.Info("Listening and serving HTTP on :127.0.0.1:2021")
 
 	lis, err := net.Listen("tcp", "127.0.0.1:2021")
 	if err != nil {
-		log.Fatalf("failed to listen: %v \n", err)
+		sugar.Info("failed to listen: %v \n", err)
 	}
 	gRpcServer := grpc.NewServer()
 	proto.RegisterTokenAuthServiceServer(gRpcServer, &Server{})
 
 	if err := gRpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v \n", err)
+		sugar.Info("failed to serve: %v \n", err)
 	}
 }
