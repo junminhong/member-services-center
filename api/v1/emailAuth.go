491f99999e2484123235adee054a4e3b37485a54
junminhong <junminhong1110@gmail.com>
2022-01-21 17:22:33 +0800

    fix 信箱驗證功能，順便調整一些邏輯判斷

--- api/v1/emailAuth.go
+++ api/v1/emailAuth.go
@@ -4,52 +4,53 @@ import (
 	"context"
 	"github.com/junminhong/member-services-center/db/redis"
 	"github.com/junminhong/member-services-center/model"
-	"log"
-	"net/http"
+	"github.com/junminhong/member-services-center/pkg/handler"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-var ctx = context.Background()
-
-type emailAuthReq struct {
-	EmailToken string `form:"emailToken" json:"emailToken" binding:"required"`
-}
+var redisClient = redis.Setup()
 
 func updateEmailAuth(email string) bool {
 	err := postgresDB.Model(&model.Member{}).Where("email = ?", email).Update("email_auth", true).Error
+	if err != nil {
+		sugar.Info(err.Error())
+	}
 	return err != nil
 }
 
 func VerifyEmail(c *gin.Context) {
-	req := &emailAuthReq{}
-	err := c.BindJSON(req)
+	email, err := redisClient.Get(context.Background(), c.Param("emailToken")).Result()
 	if err != nil {
-		log.Println(err.Error())
+		sugar.Info(err.Error())
 	}
-	redisClient := redis.InitRedis()
-	email, err := redisClient.Get(ctx, req.EmailToken).Result()
-	if err != nil {
-		log.Println(err.Error())
-	}
-	if email != "" {
-		if !updateEmailAuth(email) {
-			c.JSON(http.StatusOK, gin.H{
-				"result":  "200",
-				"message": "認證成功",
-			})
-		} else {
-			c.JSON(http.StatusOK, gin.H{
-				"result":  "400",
-				"message": "完蛋～資料更新失敗！",
-			})
-		}
-	} else {
-		c.JSON(http.StatusOK, gin.H{
-			"result":  "400",
-			"message": "你是誰？請不要亂來驗證信箱好嗎！",
+	if email == "" {
+		c.JSON(handler.BadRequest, handler.Response{
+			ResultCode: handler.BadRequest,
+			Message:    "驗證信箱連結已經失效",
+			Data:       "",
+			TimeStamp:  time.Now().UTC(),
 		})
 		return
 	}
-
+	if updateEmailAuth(email) {
+		c.JSON(handler.BadRequest, handler.Response{
+			ResultCode: handler.BadRequest,
+			Message:    "信箱驗證失敗",
+			Data:       "",
+			TimeStamp:  time.Now().UTC(),
+		})
+		return
+	}
+	err = redisClient.Del(context.Background(), c.Param("emailToken")).Err()
+	if err != nil {
+		sugar.Info(err.Error())
+	}
+	c.JSON(handler.OK, handler.Response{
+		ResultCode: handler.OK,
+		Message:    "信箱驗證成功",
+		Data:       "",
+		TimeStamp:  time.Now().UTC(),
+	})
 }
