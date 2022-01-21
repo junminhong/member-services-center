76e40db2267f013d6897536b79ef41a834aac746
junminhong <junminhong1110@gmail.com>
2022-01-21 17:23:21 +0800

    fix member register

--- api/v1/member.go
+++ api/v1/member.go
@@ -3,7 +3,9 @@ package v1
 import (
 	"github.com/junminhong/member-services-center/db/postgresql"
 	"github.com/junminhong/member-services-center/model"
+	"github.com/junminhong/member-services-center/pkg/handler"
 	"github.com/junminhong/member-services-center/pkg/jwt"
+	"github.com/junminhong/member-services-center/pkg/logger"
 	"github.com/junminhong/member-services-center/pkg/smtp"
 	"log"
 	"net/http"
@@ -14,6 +16,7 @@ import (
 )
 
 var postgresDB = postgresql.GetDB()
+var sugar = logger.Setup()
 
 type registerReq struct {
 	Email       string `form:"email" json:"email" binding:"required"`
@@ -21,50 +24,71 @@ type registerReq struct {
 	RepPassword string `form:"rep-password" json:"rep-password" binding:"required"`
 }
 
-type response struct {
-	Message string `json:"message"`
-}
-
+// Register
 // @Summary 註冊會員帳號
 // @Tags member
 // @version 1.0
 // @Accept application/json
 // @produce application/json
 // @param data body registerReq true "註冊資料"
-// @Success 200 {object} response "{"message":""}"
+// @Success 200 {object} handler.Response
 // @Router /member/register [post]
 func Register(c *gin.Context) {
-	req := &registerReq{}
-	err := c.BindJSON(req)
+	request := &registerReq{}
+	err := c.BindJSON(request)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "請傳送正確資料",
+		c.JSON(handler.BadRequest, handler.Response{
+			ResultCode: handler.BadRequest,
+			Message:    handler.ResponseFlag[handler.BadRequest],
+			Data:       "",
+			TimeStamp:  time.Now().UTC(),
 		})
 		return
 	}
-	if strings.Compare(req.Password, req.RepPassword) != 0 {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "請傳送正確資料",
+	if strings.Compare(request.Password, request.RepPassword) != 0 {
+		c.JSON(handler.BadRequest, handler.Response{
+			ResultCode: handler.BadRequest,
+			Message:    "密碼不匹配，請重新請求",
+			Data:       "",
+			TimeStamp:  time.Now().UTC(),
 		})
 		return
 	}
+	response := registerHandler(request)
+	if response.ResultCode != handler.OK {
+		c.JSON(response.ResultCode, response)
+		return
+	}
+	smtp.SendEmailAuth(request.Email)
+	c.JSON(response.ResultCode, response)
+}
+
+func registerHandler(request *registerReq) *handler.Response {
 	memberStruct := &model.Member{}
 	memberStruct.CreatedAt = time.Now().UTC()
 	memberStruct.UpdatedAt = time.Now().UTC()
 	memberStruct.ActivatedAt = time.Now().UTC()
-	memberStruct.Email = req.Email
-	memberStruct.Password = req.Password
+	memberStruct.Email = request.Email
+	memberStruct.Password = request.Password
 	result := postgresDB.Create(memberStruct)
+	var response *handler.Response
 	if result.Error == nil {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "註冊成功",
-		})
-		smtp.SendEmailAuth(req.Email)
+		response = &handler.Response{
+			ResultCode: handler.OK,
+			Message:    "帳戶註冊成功",
+			Data:       "",
+			TimeStamp:  time.Now().UTC(),
+		}
 	} else {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "註冊失敗",
-		})
+		response = &handler.Response{
+			ResultCode: handler.BadRequest,
+			Message:    "帳戶註冊失敗",
+			Data:       "",
+			TimeStamp:  time.Now().UTC(),
+		}
+		sugar.Info(result.Error)
 	}
+	return response
 }
 
 type loginReq struct {
